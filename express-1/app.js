84804dfc60d213c12a2da0a5e0d341bb0228290a
ShreyLakhataria <SLakhataria@idssoft.com>
2025-07-22 11:02:06 +0530

    express app with EJS template engine

--- express-1/app.js
+++ express-1/app.js
@@ -9,7 +9,7 @@ app.get('/', (req, res) => {
 })
 
 app.get('/home', (req, res) => {
-  res.sendFile(__dirname + '/home.html')
+  res.render(`home`,{"mya" : "akash"})
 })
 
 app.get('/about', (req, res) => {
@@ -18,6 +18,35 @@ app.get('/about', (req, res) => {
 
 app.get('/contact', (req, res) => {
   res.sendFile(__dirname + '/contact.html')
+})
+
+app.get('/product/:id/' , (req , res) => {
+  var id = req.params.id;
+  res.send("product id is" +id); 
+})
+
+app.get("/search/" , (req, res) => {
+  var query = req.query.q;
+  res.send("search query value is" + query);
+})
+
+app.get("/sumdemo" , (req, res) => {
+  res.sendFile(__dirname + '/sumdemo.html');
+})
+
+app.get(`/sumprocess`, (req , res) => {
+  var q = req.query.q; 
+  var a = parseInt(req.query.num1);
+  var b = parseInt(req.query.num2);
+  var sum = a + b;
+  res.send(`Value of a is ${a} <br/> and value of b is ${b} <br/> and sum is ${sum}`);
+})
+
+app.set('views', __dirname + `/views`);
+app.set('view engine', 'ejs');
+
+app.get('/home', (req, res) => {
+  res.render('home', {"mya": "Shrey lakhataria"});
 })
 
 
